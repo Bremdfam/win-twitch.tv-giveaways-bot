d39d2a4ed201bedd3839069ee1ef6663f0730082
Brendan <bpiper3@nait.ca>
2025-09-20 23:57:56 -0600

    Added sound cue when a dupliacate message is detected and writes that message to a txt file

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
+const fs = require('fs'); // writes to text file
 const tmi = require('tmi.js');
+const player = require('play-sound')({ players: ['powershell'] });
+const path = require('path');
+const soundPath = path.resolve(__dirname, 'button-10.wav');
 
 const channels = process.env.CHANNELS.split(',');
 
@@ -17,30 +21,43 @@ client.connect();
 const message_list = [] // Array of messages from ALL chats
 let lastBotMessageTime = 0; // Timestamp of last bot message
 const COOLDOWN_MS = 30 * 60 * 1000; // 30 minutes in milliseconds
+let same_message_count = 0
 
 client.on('message', (channel, tags, message, self) => {
-    if (self || tags['user-type'] === 'bot' || tags.mod) return;
+    if (self || tags['user-type'] === 'bot' || tags.mod || tags.badges?.vip) return; // Filters out bots, mods, and my own messages
 
     message_list.push(message)
     const last_message = message_list[message_list.length - 1]
 
-    console.log(message_list, "LIST OF MESSAGES")
+    console.log(message_list)
     console.log(last_message)
+    console.log(same_message_count)
 
     if (message_list.length > 10) { message_list.shift(); } // Remove oldest message
 
 
-    if (message_list.length >= 5) {
-        const lastFive = message_list.slice(-6, -1); // Get the 5 messages before the last one
+    if (message_list.length >= 3) {
+        const lastFive = message_list.slice(-3); // Get the 5 last messages
         const allSame = lastFive.every(msg => msg === last_message); // Check if the last message is the same as the previous five
         const now = Date.now();
-        const onCooldown = now - lastBotMessageTime < COOLDOWN_MS;
+        const onCooldown = now - lastBotMessageTime < COOLDOWN_MS; // 30 minute cooldown
 
-        if (allSame && !onCooldown) {
-            console.log("The last is same as the last five:", last_message)
+
+        if (allSame) { //&& !onCooldown
+            const logEntry = `[${new Date(now).toISOString()}] [${channel}] ${message}\n`;
+            same_message_count = same_message_count + 1
+
+            fs.appendFile('message.txt', logEntry, (err) => {
+                if (err) throw err;
+                console.log('Message saved to message.txt');
+            }); // writes to message file
+
+            player.play(soundPath, (err) => {
+                if (err) console.error('Error playing sound:', err);
+            }); // play sound
+
             lastBotMessageTime = now; // Update cooldown timestamp
-            // Print the last message to chat
-            client.say(channel, last_message);
+            //client.say(channel, last_message); // Print the last message to chat
 
         } else if (onCooldown) {
             console.log("On Cool down")
